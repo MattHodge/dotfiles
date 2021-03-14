ae816e88f45d385b90b78aa788f38079325c41c6
Matthew Hodgkins <matthodge@gmail.com>
2021-03-14 22:37:37 +0100

    More dotfiles

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a arr=(".alacritty.yml" ".gitconfig" ".gitignore_global" ".tmux.conf")
+
+pushd ~
+for i in "${arr[@]}"
+do
+    echo "Checking file: $i"
+    if [ ! -f "${HOME}/${i}" ]; then
+        echo "    Linking..."
+        ln -s "${HOME}/dotfiles/${i}" "${HOME}/${i}"
+    fi
+done
+popd
